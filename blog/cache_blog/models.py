8af249afe3f39af8f3b62c1bdbb1fc58d4189773
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-11-14 15:54:07 +0200

    new app was created

---
+++ blog/cache_blog/models.py
@@ -0,0 +1,8 @@
+# models.py
+from django.db import models
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ blog/cache_blog/urls.py
@@ -0,0 +1,7 @@
+# urls.py
+from django.urls import path
+from .views import blog_post_detail
+
+urlpatterns = [
+    path('blog/<int:post_id>/', blog_post_detail, name='blog_post_detail'),
+]
