d7773035f2240cc1a8a455c3dde4107f54af9728
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-11-15 16:51:49 +0200

    refactoring of setting.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,9 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-g61el0-gv5xtaf0s&2i^k(5ne-%trfx_@u*p_y*@(hn^rauc5k"
-)
+SECRET_KEY = "django-insecure-g61el0-gv5xtaf0s&2i^k(5ne-%trfx_@u*p_y*@(hn^rauc5k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,20 +83,16 @@ CACHES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAt"
-        + "tributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.Minimu"
-        + "mLengthValidator",
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.Common"
-        + "PasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.Numeric"
-        + "PasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
