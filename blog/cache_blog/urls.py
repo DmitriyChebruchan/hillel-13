17788e6c08cde5b520556551531762c781767de5
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-11-15 16:44:54 +0200

    refactoring of setting.py

--- blog/cache_blog/urls.py
+++ blog/cache_blog/urls.py
@@ -5,5 +5,5 @@ from .views import blog_post_detail, index_page, edit_blog_post
 urlpatterns = [
     path("", index_page, name="index_page"),
     path("read_blog/<int:post_id>", blog_post_detail, name="blog_post_detail"),
-    path("edit_blog/<int:post_id>", edit_blog_post, name="edit_blog")
+    path("edit_blog/<int:post_id>", edit_blog_post, name="edit_blog"),
 ]
